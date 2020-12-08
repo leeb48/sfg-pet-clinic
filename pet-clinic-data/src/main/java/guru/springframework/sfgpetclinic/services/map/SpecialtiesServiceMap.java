6f1f5b9cb4be075afeb3bfd292a01440573a7268
Bong Lee <bongster228@github.com>
2020-12-07 22:55:24 -0800

    Create Specialty Map Service Impl. closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialtiesServiceMap.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialtiesServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+}
