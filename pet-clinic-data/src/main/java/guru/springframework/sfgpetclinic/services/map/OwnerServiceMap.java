5a95dde49057550f475573587369f1a242dcd2bb
Bong Lee <bongster228@github.com>
2020-12-07 22:10:59 -0800

    Create PetType Map Service Imple. closes #29

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -4,40 +4,13 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
-    @Override
-    public Set<Owner> findAll() {
-        return super.findAll();
-    }
 
     @Override
     public Owner findByLastName(String lastName) {
         return null;
-    }
-
-    @Override
-    public Owner findById(Long id) {
-        return super.findById(id);
-    }
-
-    @Override
-    public Owner save(Owner object) {
-        return super.save(object);
-    }
-
-
-    @Override
-    public void deleteById(Long id) {
-        super.deleteById(id);
-    }
-
-    @Override
-    public void delete(Owner object) {
-        super.delete(object);
     }
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -4,35 +4,8 @@ import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.PetService;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
 @Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
-    @Override
-    public Set<Pet> findAll() {
-        return super.findAll();
-    }
-
-    @Override
-    public Pet findById(Long id) {
-        return super.findById(id);
-    }
-
-    @Override
-    public Pet save(Pet object) {
-        return super.save(object);
-    }
-
-    @Override
-    public void deleteById(Long id) {
-        super.deleteById(id);
-    }
-
-    @Override
-    public void delete(Pet object) {
-        super.delete(object);
-
-    }
 
 }
