5815addcf29f98081925825b256e61f1afd89269
Bong Lee <bongster228@github.com>
2020-12-07 23:31:00 -0800

    Create specialties, add to vets on startup (bootstrap). closes #34

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialtiesServiceMap.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.sfgpetclinic.services.map;
-
-import guru.springframework.sfgpetclinic.model.Specialty;
-import guru.springframework.sfgpetclinic.services.SpecialtiesService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class SpecialtiesServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
-}
